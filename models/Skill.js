f9f9eb0a9931f61baa4248632f45a2ac7ed3ef3e
Matthew Canham <mdcanham@me.com>
2016-03-08 14:24:01 +1300

    Initial commit - Keystone JS generator with some slight adjustments to check if custom models will work

---
+++ models/Skill.js
@@ -0,0 +1,16 @@
+var keystone = require('keystone');
+
+/**
+ * PostCategory Model
+ * ==================
+ */
+
+var Skill = new keystone.List('Skill', {
+	autokey: { from: 'name', path: 'key', unique: true }
+});
+
+Skill.add({
+	name: { type: String, required: true }
+});
+
+Skill.register();
---
+++ routes/views/index.js
@@ -0,0 +1,34 @@
+var keystone = require('keystone');
+
+exports = module.exports = function(req, res) {
+
+	var view = new keystone.View(req, res);
+	var locals = res.locals;
+
+	// locals.section is used to set the currently selected
+	// item in the header navigation.
+	locals.section = 'home';
+
+	locals.data = {
+		skills: []
+	};
+
+	//Load in skills
+	view.on('init', function(next){
+		keystone.list('Skill').model.find().exec(function(err, results) {
+
+			if (err || !results.length) {
+				return next(err);
+			}
+
+			locals.data.skills = results;
+
+			next();
+
+		});
+	});
+
+	// Render the view
+	view.render('index');
+
+};
