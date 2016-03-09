cb77da10b303a4d73242d407f182b00ec6d8e0a0
Matthew Canham <mdcanham@me.com>
2016-03-10 12:32:37 +1300

    Moved typed.js code into seperate js file

---
+++ public/js/typed.js
@@ -0,0 +1,9 @@
+$(function(){
+    $("#typed").typed({
+        stringsElement: $('#typed-strings'),
+        typeSpeed: 80,
+        backSpeed: 20,
+        backDelay: 2000,
+        loop: true
+    });
+});
