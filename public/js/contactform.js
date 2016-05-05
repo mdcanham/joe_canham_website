27c364f27440c3f89dfb2371e19cffe45fb6bd30
Matthew Canham <mdcanham@me.com>
2016-05-05 18:16:38 +1200

    Got the contact form working

--- public/js/contactform.js
+++ public/js/contactform.js
@@ -1,4 +1,29 @@
-$('#contactform').on('submit', function(event){
+var cf = $('#contactform');
+var spinner = cf.find(".spinner");
+var button = cf.find("button");
+
+cf.on('submit', function(event){
   event.preventDefault();
-  console.log("submitted");
+
+  var data = cf.serialize();
+
+  button.text("");
+  spinner.css('display', 'block');
+
+  $.post("/contact", data, function(response, status){
+    if(status === "success"){
+      cf.find("input, textarea").val("");
+      spinner.css("display", "none");
+      cf.find("button").text(response.message);
+      setTimeout(function(){
+        cf.find("button").text("Send");
+      }, 5000);
+    } else {
+      spinner.css("display", "none");
+      cf.find("button").text(response.message);
+      setTimeout(function(){
+        cf.find("button").text("Send");
+      }, 5000);
+    }
+  }, "json");
 });
--- routes/contact.js
+++ routes/contact.js
@@ -3,16 +3,32 @@ var domain = 'mg.joecanham.co';
 var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
 
 exports = module.exports = function(app) {
-  //Allow videos to be accessed fromt the frontend
+  //Allow videos to be accessed from the frontend
   app.post('/contact', function(request, response){
+    if(request.body.honey){
+      response.status(500).send({ message: 'We think that you might be a robot! Your message was not sent.' });
+      return;
+    };
+
+    if(!request.body.name ||  request.body.name === ""
+    || !request.body.email ||  request.body.email === ""
+    || !request.body.message ||  request.body.message === ""){
+      response.status(400).send({ messsage: 'Please enter your details correctly' });
+      return;
+    }
+
     var data = {
       from: request.body.name + '<' + request.body.email + '>',
-      to: 'admin@joecanham.co',
+      to: process.env.CONTACT_EMAIL,
       subject: 'joecanham.co contact form',
       text: request.body.message
     };
     mailgun.messages().send(data, function (error, body) {
-      console.log(body);
+      if(error){
+        response.status(400).send({ messsage: 'Something went wrong, please try again.' });
+        return;
+      }
+      response.status(200).send({message: 'We\'ve got your message. Thanks!'});
     });
   });
 }
